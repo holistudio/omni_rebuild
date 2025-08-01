53dfc49e0637f3eb03bf245a65d2b528aac290bc
holistudio <daniel.bin.lu@gmail.com>
2025-08-01 11:57:01 -0400

    Create agent.py

---
+++ lang_sandbox/agent.py
@@ -0,0 +1,32 @@
+import getpass
+import os
+
+from langchain.chat_models import init_chat_model
+
+try:
+    # load environment variables from .env file (requires `python-dotenv`)
+    from dotenv import load_dotenv
+
+    load_dotenv(dotenv_path=os.path.join('..','backend','.env'))
+except ImportError:
+    pass
+
+# os.environ["LANGSMITH_TRACING"] = "true"
+os.getenv("LANGSMITH_TRACING")
+
+if not os.getenv("LANGSMITH_API_KEY"):
+    os.environ["LANGSMITH_API_KEY"] = getpass.getpass(
+        prompt="Enter your LangSmith API key (optional): "
+    )
+if not os.getenv("LANGSMITH_PROJECT"):
+    os.environ["LANGSMITH_PROJECT"] = getpass.getpass(
+        prompt='Enter your LangSmith Project Name (default = "default"): '
+    )
+    if not os.environ.get("LANGSMITH_PROJECT"):
+        os.environ["LANGSMITH_PROJECT"] = "default"
+
+if not os.getenv("GOOGLE_API_KEY"):
+  os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+
+model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
