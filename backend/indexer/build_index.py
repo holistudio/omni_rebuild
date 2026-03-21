c9eeda5cb1c26e1584ce11d68e1ea8c19246c6dd
holistudio <daniel.bin.lu@gmail.com>
2026-03-21 12:43:26 -0400

    build_document()

---
+++ backend/indexer/build_index.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+from llama_index.core import Document
+
+# assumes pwd = "./backend" NOT "./backend/indexer"
+CORPUS_PATH = os.path.join("data", "books_corpus.json")
+INDEX_DIR =  os.path.join("data", "vector_index")
+
+def build_document(corpus: list[dict]) -> list[Document]:
+    documents = []
+    for book in corpus:
+        subjects_str = ", ".join(book.get("subjects",[]))
+        text = f"{book["title"]} by {book["author"]}. Summary: {book.get("summary","")} Subjects: {subjects_str}"
+
+        metadata = {
+            "title": book["title"],
+            "author": book["author"],
+            "year": str(book.get("year", "N/A")),
+            "genre": book.get("genre", "General"),
+            "open_library_key": book.get("open_library_key", ""),
+        }
+
+        doc = Document(text=text, metadata=metadata)
+        documents.append(doc)
+    return documents
